e50ef0acff2e744aee5b4f31445c7298b521870e
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-06-25 12:10:48 +0530

    cors error handled

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
+import cors from "cors"
 import { studentRouter } from "./Routes/students.js";
 dotenv.config();
+
 const PORT = process.env.PORT
 
 // initiating a server
@@ -9,7 +11,7 @@ const app = express();
 // configuring env variables 
 //middle wares
 app.use(express.json());
-
+app.use(cors())
 //application middlewares
 app.use("/student", studentRouter)
 
