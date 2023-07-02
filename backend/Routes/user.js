9887b02adf37adc21d3c2ad4f94981674b9bc81c
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-07-02 11:08:30 +0530

    backend with complete authentication

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,30 +1,50 @@
-import express from "express"
+import express from "express";
+import bcrypt from "bcrypt";
+import { addUser, generateToken, getUser } from "../Controllers/user.js";
 const router = express.Router();
 
-
-router.post("/signup", (req, res)=>{
-    try {
-        const user = req.body
-        res.send({data:user})
-    } catch (error) {
-        console.log(error);
-        res
-        .status(500)
-        .json({ message: "Internal server error" });
+router.post("/signup", async (req, res) => {
+  try {
+    //hashing user password.
+    const salt = await bcrypt.genSalt(10);
+    const user = await getUser(req.body.email);
+    //validating if user already exist
+    if(!user){
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedUser = await { ...req.body, password: hashedPassword };
+    const result = await addUser(hashedUser);
+    // checking mongodb acknowledgement
+    if(!result.acknowledged){
+        return res.status(404).json({message:"Error uploading user information"})
     }
-})
-
-
-router.post("/login", (req, res)=>{
-    try {
-        const user = req.body
-        res.send({data:user})
-    } catch (error) {
-        console.log(error);
-        res
-        .status(500)
-        .json({ message: "Internal server error" });
+    return res.status(201).json({data:hashedUser});
     }
-})
+    res.status(400).json({message:"Email already exist"});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async(req, res) => {
+  try {
+    //user exist validations
+    const user = await getUser(req.body.email);
+    if(!user){
+        return res.status(404).json({message:"Invalid Email"})
+    }
+    // validating password
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword){
+        return res.status(400).json({message:"Invalid Password"})
+    }
+    const token = generateToken(user._id);
+    res.send({ data: user, token :token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export const userRouter = router;
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cors from "cors"
 import { studentRouter } from "./Routes/students.js";
 import { userRouter } from "./Routes/user.js";
+import { isAuthenticated } from "./Authentication/userAuth.js";
 dotenv.config();
 
 const PORT = process.env.PORT
@@ -14,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 //application middlewares
-app.use("/student", studentRouter);
+app.use("/student",isAuthenticated, studentRouter);
 app.use("/user",userRouter );
 
 // listen a server
